a637311253a8e138f2213cd745cc8a0349df69e7
Bercan Turkmen <befeturkmen@gmail.com>
2025-06-25 22:40:15 +0100

    ChattApp V0.1
    BackEnd signup using mongoDB

---
+++ backend/src/lib/utils.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken"
+
+export const genToken = (userId, res) =>{
+
+    const token = jwt.sign({userId}, process.env.JWT_SECRET,{
+        expiresIn:"7d"
+    })
+    
+    res.cookies("jwt_token", token, {
+        maxAge: 7 * 24 * 60 * 60 * 1000, //7 days in ms
+        httpOnly: true, // prevents xss attacks
+        sameSite: "strict", // prevents CSRF attacts
+        secure: process.env.NODE_ENV !== "development"
+    })
+
+    return token
+};
