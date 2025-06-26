3d59e94fc576a4f833dcd2da6aac1325ebd92627
Bercan Turkmen <befeturkmen@gmail.com>
2025-06-27 00:32:04 +0100

    V0.2.1
    started frontend connected back end to front end

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,33 @@
-import { useState } from 'react'
+import { useEffect } from 'react'
+import { Routes, Route } from 'react-router-dom'
 
-import React from 'react'
+import Navbar from './components/navbar'
+import HomePage from './pages/HomePage'
+import SignupPage from './pages/SignupPage'
+import LoginPage from './pages/LoginPage'
+import SettingsPage from './pages/SettingsPage'
+import ProfilePage from './pages/ProfilePage'
+
+import { useAuthStore } from './store/useAuthStore'
 
 const App = () => {
+  const {authUser, checkAuth} = useAuthStore()
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log("authuser: ", authUser);
   return (
     <div>
-      hello
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
     </div>
   )
 }
