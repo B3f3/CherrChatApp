1be94bd191415e7ec9a23234c4afd6f66ff1b30d
Bercan Turkmen <befeturkmen@gmail.com>
2025-06-29 18:40:39 +0100

    v0.3.4
    a

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,11 +9,13 @@ import SettingsPage from './pages/SettingsPage'
 import ProfilePage from './pages/ProfilePage'
 
 import { useAuthStore } from './store/useAuthStore'
+import { useThemeStore } from "./store/useThemeStore";
 import { Loader } from "lucide-react"
 import { Toaster } from "react-hot-toast";
 
 const App = () => {
   const {authUser, checkAuth, isCheckingAuth} = useAuthStore()
+  const { theme } = useThemeStore();
 
   useEffect(() => {
     checkAuth();
@@ -28,7 +30,7 @@ const App = () => {
   }
 
   return (
-    <div>
+    <div data-theme={theme}>
       <Navbar />
       <Routes>
         <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} /> //Route to HomePage if user not authenticated route to LoginPage
